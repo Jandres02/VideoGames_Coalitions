d4fa0a37cda4cff7df73f3cdb4b1eb974f1afe4e
Jandres02 <avjohan@uninorte.edu.co>
2022-11-06 17:20:15 -0500

    images to cc

--- src/game/App.java
+++ src/game/App.java
@@ -6,8 +6,8 @@ public class App {
         Rect_rect rr = new Rect_rect();
         Circle_circle cc = new Circle_circle();
         Rect_circle rc = new Rect_circle();
-        rr.run();
-        // cc.run();
+        // rr.run();
+        cc.run();
         // rc.run();
     }
 }
--- src/game/Circle_circle.java
+++ src/game/Circle_circle.java
@@ -1,6 +1,7 @@
 package game;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Circle_circle extends PApplet{
 
@@ -10,6 +11,7 @@ public class Circle_circle extends PApplet{
         size(500, 500);
     }
     circleL c1, c2;
+    PImage pokemon, pokeball;
     @Override
     public void setup() {
 
@@ -18,11 +20,17 @@ public class Circle_circle extends PApplet{
     @Override
     public void draw() {
         background(0,0,0);
-        c1 = new circleL(mouseX,mouseY,50);
+        pokemon = loadImage("src/images/pokemon.png");
+        pokeball = loadImage("src/images/pokeball.png");
+        pokemon.resize(100,100);
+        pokeball.resize(60,60);
+        c1 = new circleL(mouseX,mouseY,65);
         c2 = new circleL(250,250,120);
         c2.display();
+        image(pokemon,203,200);
         c1.display();
-
+        image(pokeball, mouseX-30, mouseY-30);
+        
         float distance = dist(c1.x,c1.y,c2.x,c2.y);
         if((distance) < ((c1.r+c2.r)/2)){
             fill(223,14,14);
